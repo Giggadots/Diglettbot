d6ded3e59df0fdbe6381b44654961f957090ad87
Giggadots <54181866+Giggadots@users.noreply.github.com>
2019-08-16 11:12:19 -0400

    Add files via upload

---
+++ owner.py
@@ -0,0 +1,56 @@
+from discord.ext import commands
+
+class OwnerCog:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Hidden means it won't show up on the default help.
+    @commands.command(name='load', hidden=True)
+    @commands.is_owner()
+    async def cog_load(self, ctx, *, cog: str):
+        """Command which Loads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        print("\nLoad module: " + cog)
+        try:
+            self.bot.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            print("SUCCESS")
+            await ctx.message.delete()
+
+    @commands.command(name='unload', hidden=True)
+    @commands.is_owner()
+    async def cog_unload(self, ctx, *, cog: str):
+        """Command which Unloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        print("\nUnload module: " + cog)
+        try:
+            self.bot.unload_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            print("SUCCESS")
+            await ctx.message.delete()
+
+    @commands.command(name='reload', hidden=True)
+    @commands.is_owner()
+    async def cog_reload(self, ctx, *, cog: str):
+        """Command which Reloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        print("\nReload module: " + cog)
+        try:
+            self.bot.unload_extension(cog)
+            self.bot.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            print("SUCCESS")
+            await ctx.message.delete()
+
+def setup(bot):
+    bot.add_cog(OwnerCog(bot))
